bb13786d5c0b108364fb4f379ad161763de6b5f4
nettobruno <brunonettomac@outlook.com>
2019-11-27 01:59:38 -0300

    Adicionando mais itens ao extrato

--- src/components/ItemSaldo/index.js
+++ src/components/ItemSaldo/index.js
@@ -10,10 +10,24 @@ export default class ContainerItem extends React.Component {
 
     this.state = {
       records: [
-        { name: 'Burguer King', date: '27/11', hour: '17:10', price: -36.0 },
-        { name: 'Adidas', date: '27/11', hour: '18:30', price: -599.0 },
-        { name: 'Salário', date: '27/11', hour: '20:00', price: 3500.0 },
-        { name: 'Comida', date: '27/11', hour: '21:50', price: -25.0 },
+        { name: 'Burguer King', date: '25/11', hour: '17:10', price: -36.0 },
+        { name: 'Adidas', date: '25/11', hour: '18:30', price: -599.9 },
+        { name: 'Salário', date: '26/11', hour: '20:00', price: 3500.0 },
+        { name: 'Comida', date: '26/11', hour: '21:50', price: -25.0 },
+        { name: 'Lojas Renner', date: '27/11', hour: '09:02', price: -72.0 },
+        {
+          name: 'Comp Assistência',
+          date: '27/11',
+          hour: '11:34',
+          price: -136.0,
+        },
+        {
+          name: 'Cantinho Mineiro',
+          date: '27/11',
+          hour: '12:18',
+          price: -54.5,
+        },
+        { name: 'Estacionamento', date: '27/11', hour: '19:15', price: -45.0 },
       ],
     };
   }
